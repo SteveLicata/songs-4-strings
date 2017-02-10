1782d5499196be3fccf9f132786086e2b9260465
Steve Licata <Steve.Licata@gmail.com>
2017-02-10 15:07:08 -0500

    comments added

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,20 +3,20 @@ $(document).on('turbolinks:load',function(){
 
   console.log("DOM Loaded");
 
-  // set height onload && window resize
+  // set height onload && window resize for flexbox homepage image
   $(window).on("resize", function () {
     var height = $(window).height();
     // $(".mock-div").css('height', height);
     $('.flex-container').css('height', height - 64);
   }).resize();
 
-  // jQuery sortable function
+  // jQuery sortable function for setlists
   $( function() {
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
   });
 
-  // Materialize carousel function
+  // Materialize carousel function for homepage
   $('.carousel').carousel();
 
   //Navbar
